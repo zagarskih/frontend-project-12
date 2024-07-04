8f56c460ed2a6bc0d9e8ad3be38083298454c660
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2024-07-04 19:27:03 +0400

    deleted extra lines

--- frontend/src/routes/chat/chatSlice.js
+++ frontend/src/routes/chat/chatSlice.js
@@ -1,6 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { act } from "react";
 
 export const fetchChatData = createAsyncThunk(
   "chat/fetchChannelsAndMessages",
