b3059337e6d8ab5b6603751bf0955e8571d58498
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2024-07-11 00:50:32 +0400

    new image

--- frontend/src/error-page.js
+++ frontend/src/error-page.js
@@ -1,8 +1,6 @@
 import { useRouteError } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import ChatHeader from "./routes/HeaderChat";
 
 export default function ErrorPage() {
   const { t } = useTranslation();
@@ -10,21 +8,24 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div
-      className="h-100 bg-light d-flex align-items-center justify-content-center"
-      id="error-page"
-    >
-      <div className="container">
-        <div className="row d-flex align-items-center justify-content-center">
-          <div className="col-md-4">
-            <img src="/errorPage.webp" className="img-fluid" alt="Error" />
-          </div>
-          <div className="col-md-4">
-            <h1>{t("interface.notFound")}</h1>
-            <p className="text-left">
-              {t("interface.errorPageBody")}{" "}
-              <a href="/login">{t("interface.errorPageLink")}</a>
-            </p>
+    <div className="h-100 bg-light d-flex flex-column">
+      <ChatHeader />
+      <div
+        className="h-100 bg-light d-flex align-items-center justify-content-center flex-grow-1"
+        id="error-page"
+      >
+        <div className="container">
+          <div className="row d-flex align-items-center justify-content-center">
+            <div className="col-md-4">
+              <img src="/error.png" className="img-fluid" alt="Error" />
+            </div>
+            <div className="col-md-4">
+              <h1>{t("interface.notFound")}</h1>
+              <p className="text-left">
+                {t("interface.errorPageBody")}{" "}
+                <a href="/login">{t("interface.errorPageLink")}</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
